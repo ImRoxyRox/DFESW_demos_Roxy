7bab5d57e4e240ffe615dc96250e962c7c63e5ec
ImRoxyRox <rossella.ortu@gmail.com>
2022-06-15 14:36:36 +0100

    OperatorsExercise

---
+++ src/com/qa/operators/Operators.java
@@ -0,0 +1,21 @@
+package com.qa.operators;
+
+public class Operators {
+// Add
+	public void exercisePlus(int numOne, int numTwo) {
+		System.out.println(numOne + numTwo);
+   }
+// Multiply
+	public void exerciseTimes(int numOne, int numTwo) {
+		System.out.println(numOne * numTwo);
+	}
+// Subtract
+	public void exerciseMinus(int numOne, int numTwo) {
+		System.out.println(numOne - numTwo);
+	}
+// Divide
+	public void exercisesDivide(double numOne, double numTwo) {
+		System.out.println(numOne / numTwo);
+	}
+
+}
---
+++ src/com/qa/operators/Runner.java
@@ -0,0 +1,15 @@
+package com.qa.operators;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Operators op = new Operators();
+		
+		op.exercisePlus(20, 5);
+		op.exerciseTimes(10, 6);
+		op.exerciseMinus(56, 41);
+		op.exercisesDivide(18, 5);
+	}
+
+}
