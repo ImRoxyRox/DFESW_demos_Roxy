604b8a525bd49f323a4285a3076d4e0967a2dd54
ImRoxyRox <rossella.ortu@gmail.com>
2022-06-15 16:43:59 +0100

    operatorsresult

---
+++ src/com/qa/operatorsresults/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.operatorsresults;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Results res = new Results(120, 90, 100);
+		
+		res.methodOne();
+		res.methodTwo();
+	}
+
+}
